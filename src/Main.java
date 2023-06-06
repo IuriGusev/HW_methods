fd30017d189feea81f0cda4b6cab31e0bb370960
iurii_gusev <gusevyuv@gmail.com>
2023-06-06 11:19:42 -0400

    HW_methods was made

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         printIsLeap(2022);
         printMessage(2022, 0);
-        printCalculateDeliveryDays (200);
+        printCalculateDeliveryDays (110);
 
     }
         //Task 1
@@ -18,13 +18,13 @@ public class Main {
         //Task 2
         public static int printMessage (int productionYear, int OS){
             if (OS == 0) {
-                if (productionYear <= 2015) {
+                if (productionYear < 2015) {
                     System.out.println("Установить облегченную версия IOS");
                 } else {
                     System.out.println("Установить IOS по ссылке");
                 }
             } else {
-                if (productionYear <= 2015) {
+                if (productionYear < 2015) {
                     System.out.println("Установить облегченную версию Android");
                 } else {
                     System.out.println("Установить Android по ссылке");
@@ -35,14 +35,11 @@ public class Main {
 
     //Task 3
     public static void printCalculateDeliveryDays(int deliveryDistance) {
-        deliveryDistance = 95;
             int intervalDistance = 40;
             int startDistance = 20;
             int deliveryDays = 1;
             if (deliveryDistance > 100) {
                 System.out.println("Доставка не осуществляется");
-            } if (deliveryDistance <= startDistance) {
-            deliveryDays = 1;
         } else {
             deliveryDays = deliveryDays + ((deliveryDistance - startDistance) / intervalDistance) + 1;
         }
